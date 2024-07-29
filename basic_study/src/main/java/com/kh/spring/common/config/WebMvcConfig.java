8a57c2fbb6ca680dfa35b3a0f27c023aa3c15e61
hosun <hosunjerry@naver.com>
2024-07-29 11:33:26 +0900

    파일관련내용추가 코드추가

---
+++ basic_study/src/main/java/com/kh/spring/common/config/WebMvcConfig.java
@@ -0,0 +1,15 @@
+package com.kh.spring.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer{
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**") // 매핑 URI 설정 예시로 image/uploadfiles...
+				.addResourceLocations("file:///C:/uploadFiles/"); // 자기 파일경로 써주세용
+	}
+}
