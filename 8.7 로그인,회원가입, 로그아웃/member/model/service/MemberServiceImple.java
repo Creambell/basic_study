d5735ae86d6ea2f5dca3c7e2f3656467bad420e8
euneundevdev <euneundevdev@gmail.com>
2024-08-07 17:35:27 +0900

    Add files via upload

---
+++ 8.7 로그인,회원가입, 로그아웃/member/model/service/MemberServiceImple.java
@@ -0,0 +1,30 @@
+package com.kh.spring.member.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kh.spring.member.model.dao.MemberMapper;
+import com.kh.spring.member.model.vo.Member;
+
+@Service
+public class MemberServiceImple implements MemberService {
+	
+	@Autowired
+	private MemberMapper mMapper;
+
+	@Override
+	public Member login(Member u) {
+		return mMapper.login(u);
+	}
+
+	@Override
+	public int insertMember(Member m) {
+		return mMapper.insertMember(m);
+	}
+
+	@Override
+	public int checkId(String userId) {
+		return mMapper.checkId(userId);
+	}
+
+}
