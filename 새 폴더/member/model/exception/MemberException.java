6170e72f0e49ed01fe461082ec74002c61bdc98e
euneundevdev <euneundevdev@gmail.com>
2024-08-07 17:34:57 +0900

    Delete 새 폴더 directory

--- 새 폴더/member/model/exception/MemberException.java
+++
@@ -1,6 +0,0 @@
-package com.kh.spring.member.model.exception;
-
-public class MemberException extends RuntimeException {
-	public MemberException() {}
-	public MemberException(String msg) {super(msg);}
-}
